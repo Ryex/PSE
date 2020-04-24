2fbec9d8a2870dacbebf7f7da2af8167fb4e1b76
Rachel Powers <powers.e.rachel@gmail.com>
2020-04-24 02:38:01 -0600

    arrows move to targets

---
+++ PSE_Unity/Assets/Scripts/NoteSpawner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteSpawner : MonoBehaviour
+{
+    public float beatTempo;
+    public bool trackPlaying;
+
+    private float updateRate;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        updateRate = beatTempo / 60f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if(!trackPlaying) {
+
+            if(Input.anyKeyDown) {
+                trackPlaying = true;
+                // TODO PLAY TRACK
+            }
+        }
+
+    }
+}
